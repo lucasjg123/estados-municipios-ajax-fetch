dc7fbcd7e581e31d82dc329fc199b60c6d64d936
Lucas Javier Godoy <113483967+lucasjg123@users.noreply.github.com>
2024-02-21 18:33:52 -0300

    Update municipios.php

--- municipios.php
+++ municipios.php
@@ -3,6 +3,6 @@
     $municipios = [];
     if(isset($_GET['id_estado'])) {
         $tabla_minicipio = new Municipio();
-        $municipios = $tabla_minicipio->obtener_municipios_select($_GET['id_estado']);
+        $municipios = $tabla_minicipio->obtener_municipios_select($_GET['id_estado']); // Esto es el array de municipios retornado por la BD en crudo
     }//end if
     echo json_encode(['data' => $municipios]);
